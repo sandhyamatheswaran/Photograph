009c7a28e0a8da8f529a2f2700287a6d5a609f7a
sandhyamatheswaran <118278588+sandhyamatheswaran@users.noreply.github.com>
2023-10-17 20:57:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const mainImage1 = document.getElementById("main-image1");
+    
+mainImage1.addEventListener("click", function () {
+  const newPage = window.open( "bird.html");
+
+});
+const mainImage2 = document.getElementById("main-image2");
+    
+mainImage2.addEventListener("click", function () {
+  const newPage = window.open( "function.html");
+
+});
+const mainImage3 = document.getElementById("main-image3");
+    
+mainImage3.addEventListener("click", function () {
+  const newPage = window.open( "Monkey.html");
+
+});
+const mainImage4 = document.getElementById("main-image4");
+    
+mainImage4.addEventListener("click", function () {
+  const newPage = window.open( "butterfly.html");
+
+});
+const mainImage5 = document.getElementById("main-image5");
+    
+mainImage5.addEventListener("click", function () {
+  const newPage = window.open( "Dogs.html");
+
+});
+const mainImage6 = document.getElementById("main-image6");
+    
+mainImage6.addEventListener("click", function () {
+  const newPage = window.open( "flowers.html");
+
+});
+const mainImage7 = document.getElementById("main-image7");
+    
+mainImage7.addEventListener("click", function () {
+  const newPage = window.open( "human.html");
+
+});
+const mainImage8 = document.getElementById("main-image8");
+    
+mainImage8.addEventListener("click", function () {
+  const newPage = window.open( "nature.html");
+
+});
+const mainImage9 = document.getElementById("main-image9");
+    
+mainImage9.addEventListener("click", function () {
+  const newPage = window.open( "others.html");
+
+});
